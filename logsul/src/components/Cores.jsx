f0f11af11c472bf4af53222783df53c5781f17c9
marcelo <marceloolimpiofbc@gmail.com>
2023-03-02 20:17:11 -0300

    ver05

--- logsul/src/components/Cores.jsx
+++ logsul/src/components/Cores.jsx
@@ -17,6 +17,7 @@ export default class Cores{
     static cinza3  ="#6A95B1"
     static cinza4  ="#7DA3BB"
     static cinza5  ="#8FB0C4"
+    static corMar  ="#9CC0FA"
 
     //-------------------brancas
     static branca1  ="#F7FCFC"
@@ -26,6 +27,7 @@ export default class Cores{
     static boxShadow2 = "-2px 3px 3px -4px #1D3557"
     static boxShadow3 = "-2px -4px 2px -4px #000000;"
     static textoShadow= "-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5);"
+    static filterShadow= 'drop-shadow(2px 1px 2px #1D3557);'
     
     
 
--- logsul/src/pages/Local.jsx
+++ logsul/src/pages/Local.jsx
@@ -1,8 +1,72 @@
-
+import styled from "styled-components";
+import cor from '../components/Cores';
+import mapa1 from '../assets/img/mapa.jpg';
+import cuitiba from '../assets/img/ico/seta curitiba.svg';
+import floripa from '../assets/img/ico/seta floripa.svg'
 export default function Local(){
+    const Mapa = styled.div`
+    background-color: ${cor.corMar};
+    display: flex;
+    flex-direction: row;
+         height: 80vh;
+     img{
+        width: 60vw;
+        
+     }
+     .container{
+        display: flex;
+        flex-direction:column;
+        width: 40vw;
+        height: 80vh;  
+     }
+     .localTitle{
+        display: flex;
+        flex-direction:row;
+         align-items: center;
+         justify-content: center;
+         height: 5vh;
+         font-size: 1vw;
+         font-weight:bold;
+         color: ${cor.azul1};
+         text-shadow: ${cor.textoShadow};
+     }
+     .localContent{
+
+     }
+     .ico{
+        position: absolute;
+      } 
+      .seta{
+        width: 5vw;
+      } 
+     .curitiba{
+        left: 20vw;
+        animation: curitiba 1s infinite;
+        @keyframes curitiba {
+            0%{top:12vh;};
+            50%{top: 12.5vh;};
+            100%{top: 12vh;};
+        }
+        filter: ${cor.filterShadow};
+     }
+    `
     return(<>
     <title>LOGSUL LOCALIZAÇÃO</title>
-    LOCALIZAÇÃO
+    
+    <Mapa>
+     <img src={cuitiba} alt="" className="curitiba ico seta" />
+     <img src={floripa} alt="" className="floripa ico seta" />
+     <img src={mapa1} alt="" />
+     <div className="container">
+        <div className="localTitle">
+        LOCALIZAÇÃO
+        </div>
+        <div className="localContent">
+
+        </div>
+     </div>
+    </Mapa>
+   
     
     </>)
 }
